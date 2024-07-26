98665261092fd4bcba72d2bc50f36e67bf8834c1
mobiledaisy <morana110900@gmail.com>
2024-07-26 17:49:12 +0300

    make build

---
+++ index.a7abdf7c.js
@@ -0,0 +1,2 @@
+document.querySelector("table");var t=document.querySelectorAll("tr"),e=t.length;!function(l){for(var n=0;n<e;n++){var o="",r="",c=t[n];o=c.cells[1].textContent,r=c.cells[4].textContent,c.cells[4].textContent=o,c.insertCell(5).textContent=r}}(0);
+//# sourceMappingURL=index.a7abdf7c.js.map
